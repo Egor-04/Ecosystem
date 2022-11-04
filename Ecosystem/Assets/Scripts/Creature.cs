78700fc6e026796dad88047737be3cb8a90d50e3
Egor-04 <egor04m@gmail.com>
2022-11-04 23:58:10 +0300

    Creatures, Carnivores, Herbivores, Herbs

--- Ecosystem/Assets/Scripts/Creature.cs
+++ Ecosystem/Assets/Scripts/Creature.cs
@@ -1,43 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public enum Targets {Canivores, Herbivores} //Плотоядные и Травоядные
-public class Creature : MonoBehaviour
+public enum CreatureType {Carnivores, Herbivores, Herb} //Плотоядные и Травоядные
+public abstract class Creature : MonoBehaviour
 {
     [Header("Creature Stats")]
-    [SerializeField] private float _health = 100f;
-    [SerializeField] private float _hunger = 100f;
-    [SerializeField] private float _fatigue = 100f;
+    [SerializeField] private CreatureType _creatureType;
+    [SerializeField] private float _health = 100f; // Здоровье
+    [SerializeField] private float _hunger = 100f; // Голод
+    [SerializeField] private NavMeshAgent _agent;
 
     [Header("Creature Sub-Stats")]
     [SerializeField] private float _speed;
     [SerializeField] private float _attackPower;
-    [SerializeField] private Transform _target;
-    [SerializeField] private CharacterController _characterController;
+    
+    [Header("Target Point")]
+    [SerializeField] private Vector3 _target;
+
+    [SerializeField] private float _minX, _maxX;
+    [SerializeField] private float _minZ, _maxZ;
+    [SerializeField] private float _radius = 10f;
+    [SerializeField] private Color _color;
+    private Creature _findedCreature;
 
     private void Start()
     {
         _speed = Random.Range(5f, 10f);
         _attackPower = Random.Range(0f, 10f);
-        _characterController = GetComponent<CharacterController>();
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        SelectAction();
+        DoAction();
     }
 
-    private void SelectAction()
+    private void DoAction()
     {
-        if (_target == null)
+        if (_hunger > 50f)
         {
-
+            CreateMovementPoint();
+            MoveTo();
         }
     }
 
-    private void CreateTarget()
+    private void CreateMovementPoint()
     {
+        if (_target == Vector3.zero)
+        {
+            _target = new Vector3(Random.Range(_minX, _maxX), 1f, Random.Range(_minZ, _maxZ));
+            Debug.LogError("Target");
+        }
+    }
 
+    private void MoveTo()
+    {
+        if (_target == Vector3.zero)
+        {
+            _agent.SetDestination(_target);
+
+            float distance = (_target - transform.position).sqrMagnitude;
+            
+            if (distance < 6f)
+            {
+                _target = Vector3.zero;
+                return;
+            }
+        }
+    }
+
+    private Creature FindFoodTarget()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _radius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders != null)
+            {
+                _findedCreature = colliders[Random.Range(0, colliders.Length)].GetComponent<Creature>();
+            }
+            else
+            {
+                CreateMovementPoint();
+            }
+        }
+        return _findedCreature;
+    }
+
+    public CreatureType GetCreatureType()
+    {
+        return _creatureType;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+        Gizmos.DrawWireSphere(transform.position, _radius);
     }
 }
