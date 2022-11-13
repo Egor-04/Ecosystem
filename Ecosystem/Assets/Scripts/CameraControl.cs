8fe8f38037a6b9cd02346fd5e6f6d22fb43707ea
Egor-04 <egor04m@gmail.com>
2022-11-13 18:54:47 +0300

    Camera Control

---
+++ Ecosystem/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Vector3 _startMousePosition;
+    [SerializeField] private Vector3 _currentMousePos;
+    [SerializeField] private Vector3 _cameraNewPosition;
+
+    [Header("Sensitivity")]
+    [SerializeField] private float _sensitivity = 5f;
+
+    [Header("Confines")]
+    [SerializeField] private float _minX = -100, _maxX = 100;
+    [SerializeField] private float _minZ = -100, _maxZ = 100;
+
+    [Header("Camera State")]
+    public bool IsLock;
+
+    private float _newXPos;
+    private float _newZPos;
+
+    private float _oldXPos;
+    private float _oldZPos;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _startMousePosition = Input.mousePosition;
+            _oldXPos = _newXPos;
+            _oldZPos = _newZPos;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            _currentMousePos = Input.mousePosition;
+            _newXPos = _oldXPos - (_currentMousePos.x - _startMousePosition.x) * _sensitivity / 100;
+            _newZPos = _oldZPos - (_currentMousePos.y - _startMousePosition.y) * _sensitivity / 100;
+            _cameraNewPosition = new Vector3(_newXPos, _camera.transform.position.y, _newZPos);
+
+            _camera.transform.position = _cameraNewPosition;
+        }
+    }
+}
--- Ecosystem/Assets/Scripts/StatsView.cs
+++ Ecosystem/Assets/Scripts/StatsView.cs
@@ -6,6 +6,7 @@ public class StatsView : MonoBehaviour
     [SerializeField] private TMP_Text _health;
     [SerializeField] private TMP_Text _hunger;
     [SerializeField] private TMP_Text _joy;
+    [SerializeField] private Canvas _statCanvas;
     [SerializeField] private Creature _creature;
 
     private void Start()
@@ -18,5 +19,17 @@ public class StatsView : MonoBehaviour
         _health.text = "Health: " + _creature.Health;
         _hunger.text = "Hunger: " + _creature.Hunger;
         _joy.text = "Joy: " + _creature.JoyPercent;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_statCanvas.gameObject.activeSelf)
+        {
+            _statCanvas.gameObject.SetActive(false);
+        }
+        else
+        {
+            _statCanvas.gameObject.SetActive(true);
+        }
     }
 }
