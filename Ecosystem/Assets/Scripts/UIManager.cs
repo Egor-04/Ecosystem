56ab60aaa54402c1e68d4efff65509b52cab7b6f
Egor-04 <egor04m@gmail.com>
2022-12-24 00:44:50 +0300

    Menu

--- Ecosystem/Assets/Scripts/UIManager.cs
+++ Ecosystem/Assets/Scripts/UIManager.cs
@@ -3,9 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
     public void ReloadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
